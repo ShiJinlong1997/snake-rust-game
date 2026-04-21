d4f1250a4c48ddaea70147d6f6289025ad046402
ShiJinlong1997 <853952154@qq.com>
2026-04-22 07:48:36 +0800

    feat: enum Direction loop move test

--- src/lib.rs
+++ src/lib.rs
@@ -4,13 +4,68 @@ use wee_alloc::WeeAlloc;
 #[global_allocator]
 static ALLOC: WeeAlloc = WeeAlloc::INIT;
 
-#[wasm_bindgen]
-pub fn greet(name: &str) {
-  alert(name);
-  // println!("Hi there{}", name);
+#[derive(PartialEq)]
+enum Direction {
+    Up,
+    Right,
+    Down,
+    Left,
+}
+
+struct SnakeCell(usize);
+
+struct Snake {
+    body: Vec<SnakeCell>,
+    dir: Direction,
+}
+
+impl Snake {
+    fn new(spawn_index: usize) -> Self {
+        return Self {
+            body: vec![SnakeCell(spawn_index)],
+            dir: Direction::Right,
+        };
+    }
 }
 
 #[wasm_bindgen]
-extern {
-  pub fn alert(s: &str);
+pub struct World {
+    width: usize,
+    size: usize,
+    snake: Snake,
+}
+
+#[wasm_bindgen]
+impl World {
+    pub fn new(width: usize, snake_idx: usize) -> Self {
+        return Self {
+            width,
+            size: width * width,
+            snake: Snake::new(snake_idx),
+        };
+    }
+
+    pub fn width(&self) -> usize {
+        return self.width;
+    }
+
+    pub fn snake_head_idx(&self) -> usize {
+        return self.snake.body[0].0;
+    }
+
+    pub fn update(&mut self) {
+        let snake_idx = self.snake_head_idx();
+        // 0 <= 下标范围 < 64
+        // self.snake.body[0].0 = (snake_idx - 1) % self.size;
+
+        if (Direction::Right == self.snake.dir) {
+            self.snake.body[0].0 = (snake_idx + 1) % self.size;
+        } else if (Direction::Left == self.snake.dir) {
+            self.snake.body[0].0 = (snake_idx - 1) % self.size;
+        } else if (Direction::Up == self.snake.dir) {
+            self.snake.body[0].0 = (snake_idx - self.width) % self.size;
+        } else if (Direction::Down == self.snake.dir) {
+            self.snake.body[0].0 = (snake_idx + self.width) % self.size;
+        }
+    }
 }
--- www/main.js
+++ www/main.js
@@ -1,12 +1,67 @@
-import init, { greet } from 'snake_game';
+import init, { World } from 'snake_game';
 
-async function start() {
-  const wasm = await init();
-  console.log(wasm.greet);
-  console.log(greet);
-  // debugger
-  greet('Loong');
-  console.log('OK');
+function start() {
+  const CELL_SIZE = 20;
+  const WORLD_WIDTH = 8;
+  const SNAKE_SPAWN_IDX = Date.now() % Math.pow(WORLD_WIDTH,2);
+  
+  const world = World.new(WORLD_WIDTH, SNAKE_SPAWN_IDX);
+  
+  /** @type {HTMLCanvasElement} */
+  const canvas = document.getElementById('snake-canvas');
+  const ctx = canvas.getContext('2d');
+  canvas.height = WORLD_WIDTH * CELL_SIZE;
+  canvas.width = WORLD_WIDTH * CELL_SIZE;
+
+  function drawWorld() {
+    ctx.beginPath();
+
+    // 画竖线
+    for (let x = 0; x <= WORLD_WIDTH; x += 1) {
+      ctx.moveTo(CELL_SIZE * x, 0);
+      ctx.lineTo(CELL_SIZE * x, WORLD_WIDTH * CELL_SIZE);
+    }
+
+    // 画横线
+    for (let y = 0; y <= WORLD_WIDTH; y += 1) {
+      ctx.moveTo(0, CELL_SIZE * y);
+      ctx.lineTo(WORLD_WIDTH * CELL_SIZE, CELL_SIZE * y);
+    }
+
+    ctx.stroke();
+  }
+
+  function drawSnake() {
+    const snakeIdx = world.snake_head_idx();
+    const col = snakeIdx % WORLD_WIDTH;
+    const row = Math.floor(snakeIdx / WORLD_WIDTH);
+    ctx.beginPath();
+    ctx.fillRect(col * CELL_SIZE, row * CELL_SIZE, CELL_SIZE, CELL_SIZE);
+    ctx.stroke();
+  }
+
+  function paint() {
+    drawWorld();
+    drawSnake();
+  }
+
+  function update() {
+    const fps = 10;
+    setTimeout(() => {
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
+      world.update();
+      paint();
+      requestAnimationFrame(update);
+    }, 1000 / fps);
+  }
+
+  paint();
+  update();
 }
 
-start();
+async function main() {
+  init()
+  .then(start);
+}
+
+main();
